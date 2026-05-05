c5b60ec34b1f1fe10f849a314c442e4d1dbd36bf
Muhammadyafiheldy <m.yafiheldy@gmail.com>
2026-05-05 23:52:41 +0700

    user static page

--- lib/screen/user/dashboard_screen.dart
+++ lib/screen/user/dashboard_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
 import 'history_screen.dart';
-import '../profile/profile_screen.dart'; 
+import 'profile_screen.dart'; 
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({Key? key}) : super(key: key);
